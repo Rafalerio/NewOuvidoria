365119881032390607353e8f908e4936bfc52928
Rafael Araujo Valério <128901204+Rafalerio@users.noreply.github.com>
2025-06-18 10:15:04 -0300

    Update ConexaoBanco.java

--- src/ConexaoBanco.java
+++ src/ConexaoBanco.java
@@ -2,7 +2,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class CogitnexaoBanco {
+public class ConexaoBanco {
     private static final String URL = "jdbc:mysql://localhost:3306/banco_feedback";
     private static final String USUARIO = "usuario";
     private static final String SENHA = "senha";
